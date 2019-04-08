71cc52b6c0cfdd65cbd3c401c9a90f0dd1a7b969
Seth Davenport <seth.e.davenport@gmail.com>
2019-04-08 19:27:27 -0400

    wrapper and hook variants

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 // @flow
 import React, { Component } from 'react';
 import './app.css';
-import { Counter } from './counter';
+// import { Counter } from './counter.with-behaviour-as-wrapper-component';
+import { Counter } from './counter.with-behaviour-as-custom-hook';
 
 export const App = () => (
   <div className="App">
